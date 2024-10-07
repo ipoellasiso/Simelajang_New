720a7245844d629a69fe544d3bb6c3708017df0e
ipoellasiso <ipoellasiso86@gmail.com>
2024-10-07 17:09:37 +0800

    up

--- app/Http/Controllers/TarikpajakController.php
+++ app/Http/Controllers/TarikpajakController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sp2dModel;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class TarikpajakController extends Controller
 {
@@ -24,8 +26,42 @@ class TarikpajakController extends Controller
         return view('TarikPajak.Pajakls', $data);
     }
 
-    public function save_json()
+    public function save_json(Request $request)
     {
+        // Validasi input
+        $request->validate([
+            'data' => 'required|json', // Pastikan input adalah JSON yang valid
+        ]);
+
+        // Simpan data JSON ke dalam database
+        // $post = DB::table('sp2d')->get();
+        // $post->data = $request->input('data');
+        $datasp2d = new Sp2dModel();
+        $datasp2d->idhalaman = $nomoracak;
+        $datasp2d->jenis = $request["jenis"];
+        $datasp2d->tahun = $dt["ls"]["header"]["tahun"];
+        $datasp2d->nomor_rekening = $dt["ls"]["header"]["nomor_rekening"];
+        $datasp2d->nama_bank = $dt["ls"]["header"]["nama_bank"];
+        $datasp2d->nomor_sp2d = $dt["ls"]["header"]["nomor_sp_2_d"];
+        $datasp2d->tanggal_sp2d = Carbon::Parse($dt["ls"]["header"]["tanggal_sp_2_d"])->format('Y-m-d');
+        $datasp2d->nama_skpd = $dt["ls"]["header"]["nama_skpd"];
+        $datasp2d->nama_sub_skpd = $dt["ls"]["header"]["nama_sub_skpd"];
+        $datasp2d->nama_pihak_ketiga = $dt["ls"]["header"]["nama_pihak_ketiga"];
+        $datasp2d->no_rek_pihak_ketiga = $dt["ls"]["header"]["no_rek_pihak_ketiga"];
+        $datasp2d->nama_rek_pihak_ketiga = $dt["ls"]["header"]["nama_rek_pihak_ketiga"];
+        $datasp2d->bank_pihak_ketiga = $dt["ls"]["header"]["bank_pihak_ketiga"];
+        $datasp2d->npwp_pihak_ketiga = $dt["ls"]["header"]["npwp_pihak_ketiga"];
+        $datasp2d->keterangan_sp2d = $dt["ls"]["header"]["keterangan_sp2d"];
+        $datasp2d->nilai_sp2d = $dt["ls"]["header"]["nilai_sp2d"];
+        $datasp2d->nomor_spm =  $dt["ls"]["header"]["nomor_spm"];
+        $datasp2d->tanggal_spm = Carbon::Parse( $dt["ls"]["header"]["tanggal_spm"])->format('Y-m-d');
+        $datasp2d->nama_ibu_kota = $dt["ls"]["header"]["nama_ibu_kota"];
+        $datasp2d->nama_bud_kbud = $dt["ls"]["header"]["nama_bud_kbud"];
+        $datasp2d->jabatan_bud_kbud = $dt["ls"]["header"]["jabatan_bud_kbud"];
+        $datasp2d->nip_bud_kbud = $dt["ls"]["header"]["nip_bud_kbud"];
         
+        $datasp2d->save();
+
+        return redirect()->back()->with('success', 'Data berhasil disimpan!');
     }
 }
--- resources/views/TarikPajak/Pajakls.blade.php
+++ resources/views/TarikPajak/Pajakls.blade.php
@@ -8,25 +8,24 @@
         </div>
         <div class="m-t-25">
 
-        <form method="POST">
+        <form method="POST" action="{{ url('simpanjson') }}">
         @csrf
-            <div class="card">
+        <div class="card">
             <div class="card-body flex flex-col p-6">
                 <div class="card-text h-full space-y-4">
-                        <div class="input-area">
-                            <label for="url" class="form-label">Isi Data Json </label>
-                            <!-- <textarea name="textarea" rows="5" cols="40">Write something here</textarea> -->
-                            <textarea id="jsontextarea" name="" type="text" class="form-control" rows="21"></textarea>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="submit" id="saveBtn" value="create" class="btn btn-secondary">
-                                <i class="fa fa-save"></i>  Simpan
-                            </button>
-
-                        </div>
+                    <div class="input-area">
+                        <label for="url" class="form-label">Isi Data Json </label>
+                        <!-- <textarea name="textarea" rows="5" cols="40">Write something here</textarea> -->
+                        <textarea id="jsontextarea" name="jsontextarea" type="text" class="form-control" rows="21"></textarea>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" id="saveBtn" value="create" class="btn btn-secondary">
+                            <i class="fa fa-save"></i>  Simpan
+                        </button>
+                    </div>
                 </div>
             </div>
-            </div>
+        </div>
         </form>
 
         </div>
